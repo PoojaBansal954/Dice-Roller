64cdef32b8a9aabfac734f485fe6fbfcf7c3442b
Pooja Bansal <poojabansal.cse24@satyug.edu.in>
2025-10-05 16:49:14 +0530

    changes

---
+++ Random_password_generator/script.js
@@ -0,0 +1,16 @@
+function generatePassword(length, includeLowerCase, includeUpperCase,
+                          includeNumbers,includeSymbols){
+  return '' ;
+                          }
+const passwordLength=12;
+const includeLowerCase=true;
+const includeUpperCase=true;
+const includeNumbers=true;
+const includeSymbols=true;
+
+
+
+
+const password = generatePassword(passwordLength, includeLowerCase, 
+                  includeUpperCase,includeNumbers,includeSymbols);
+console.log(`Generated password ${password}`);
