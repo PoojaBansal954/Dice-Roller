f5f5706701a884f2fd427bf6cc21c05a2af5a0f1
Pooja Bansal <poojabansal.cse24@satyug.edu.in>
2025-10-09 20:12:41 +0530

    new

--- filter/script.js
+++ filter/script.js
@@ -1,10 +1,18 @@
 // filter---create a new array by filtering out elements
 
 let number=[1,2,4,6,7,8,10];
-let evenNums=number.filter(isEven);
+let evenNums=number.filter(function (element){
+    return element%2===0;
+});
 console.log(evenNums);
 
 
-function isEven(element){
-    return element%2===0;
-}
+
+// let number=[1,2,4,6,7,8,10];
+// let evenNums=number.filter(isEven);
+// console.log(evenNums);
+
+
+// function isEven(element){
+//     return element%2===0;
+// }
